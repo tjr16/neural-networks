5020cff429b0b00398f49e61ea3a792aace3535d
jiaruit <jiaruit@kth.se>
2021-04-16 00:46:03 +0200

    feat: ensemble learning, test more hidden nodes (#1)

--- double-layer_NN/main.m
+++ double-layer_NN/main.m
@@ -33,6 +33,7 @@ GD2.cyclic = true;
 GD2.lr_max = 1e-1;
 GD2.ns = 500;
 
+% --- at least, all code above should be run ---
 %% Initialize network parameters
 [W, b] = initParam();
 nn = DoubleLayer(W, b);
@@ -88,7 +89,7 @@ if RUN_SEARCH
     l_idx = 1;
     for l = lam
         GD2.lambda = l;
-        fprintf('%d, lambda=%d\n--------------\n', l_idx, l);
+        fprintf('%d, lambda=%f\n--------------\n', l_idx, l);
         [~, metrics] = miniBatchGD(train_data, valid_data, nn);
         acc_val = metrics(end, :);
         plot(1: numel(acc_val), acc_val);
@@ -117,7 +118,7 @@ if RUN_SEARCH
     l_idx = 1;
     for l = lam
         GD2.lambda = l;
-        fprintf('%d, lambda=%d\n--------------\n', l_idx, l);
+        fprintf('%d, lambda=%f\n--------------\n', l_idx, l);
         [~, metrics] = miniBatchGD(train_data, valid_data, nn);
         acc_val = metrics(end, :);
         plot(1: numel(acc_val), acc_val);
@@ -151,3 +152,130 @@ acc_final = computeAccuracy(P_final, test_data{3});
 fprintf("Accuracy on test set: %f\n", acc_final);
 GD2 = tmp;
 % Summary: test accuracy, 52.83%
+
+
+%% performance optimization
+
+%% 1. add more hidden nodes
+tmpNN = NN2; tmpGD = GD2;
+NN2.m = 100;    % TODO: set #nodes here
+GD2.n_batch = 100; GD2.lr = 1e-5;
+GD2.cyclic = true; GD2.lr_max = 1e-1;
+GD2.ns = 2 * floor(45000/GD2.n_batch);  % stepsize <-> 2 epochs
+GD2.n_epoch = 8;    % 2 cycles <-> 4 ns <-> 8 epochs
+% init network
+[W1, b1] = initParam();
+nn1 = DoubleLayer(W1, b1);
+% search for proper regularization
+[train_data, valid_data, ~] = loadData(true, 5000);
+lam = logspace(-5, -1, 8);
+figure; % save validation acc as a figure
+l_idx = 1;
+for l = lam
+    GD2.lambda = l;
+    fprintf('%d, lambda=%f\n--------------\n', l_idx, l);
+    [~, metrics] = miniBatchGD(train_data, valid_data, nn1);
+    acc_val = metrics(end, :);
+    plot(1: numel(acc_val), acc_val);
+    hold on;
+    l_idx = l_idx + 1;
+end
+legend(string(lam));
+title('Search for lambda: more hidden nodes'); xlabel('epoch'); 
+ylabel('acc_valid', 'Interpreter', 'none');
+
+% good lr value: 3.7276e-5 (100 nodes)
+%                1.9307e-3 (150 nodes)
+
+% final test
+GD2.ns = 2 * floor(49000/GD2.n_batch);	% stepsize <-> 2 epochs
+GD2.n_epoch = 12;	% 3 cycles <-> 6 ns <-> 12 epochs
+GD2.lambda = 3.7276e-5;  % TODO: set best lr here
+[train_data, valid_data, test_data] = loadData(true, 1000);
+[nn_trained1, metrics] = miniBatchGD(train_data, valid_data, nn1);
+figure;
+plotMetrics(metrics);
+nn_final = nn_trained1.forward(test_data{1});
+P_final = nn_final.output();
+acc_final = computeAccuracy(P_final, test_data{3});
+fprintf("Accuracy on test set: %f\n", acc_final);
+NN2 = tmpNN; GD2 = tmpGD;
+
+% Summary: test accuracy, 53.05% (100 nodes)
+%                         54.84% (150 nodes)
+
+%% 2. ensemble learning for several networks
+n_model = 3;
+nns = cell(1, n_model);   % NN cell
+Ws = cell(4, n_model);    % param cells
+bs = cell(4, n_model);
+metrics = cell(1, n_model);   % metrics cell
+[train_data, valid_data, test_data] = loadData(true, 1000);
+tmp = GD2;
+GD2.n_batch = 100; GD2.lr = 1e-5;
+GD2.cyclic = true; GD2.lr_max = 1e-1;
+GD2.ns = 2 * floor(49000/GD2.n_batch);	% stepsize <-> 2 epochs
+GD2.n_epoch = 4;	% 1 cycle = 4 epochs
+GD2.lambda = 0.0021731;	% best lr
+
+for i = 1:n_model	% each model
+    fprintf('Model: %d\n', i);
+    [Ws{1, i}, b{1, i}] = initParam();
+    nns{i} = DoubleLayer(Ws{1, i}, b{1, i});
+    for j = 1:3 % each cycle: save model parameters
+        [nns{i}, metrics_] = miniBatchGD(train_data, valid_data, nns{i});
+        metrics{i} = [metrics{i}, metrics_];
+        Ws{j+1, i} = nns{i}.W;
+        bs{j+1, i} = nns{i}.b;        
+    end
+end
+% plot metrics of each model
+for i = 1:n_model
+    figure(i*1000); % distinguish different figures
+	subplotMetrics(metrics{i});
+end
+% ensemble
+accs = ensemble(nns, test_data);
+for i = 1: n_model
+    fprintf("Test accuracy, model %d: %f\n", i, accs{i});
+end
+fprintf("Test accuracy, ensemble model: %f\n", accs{end});
+GD2 = tmp;
+% Summary:
+% Test accuracy, model 1: 0.523900
+% Test accuracy, model 2: 0.523300
+% Test accuracy, model 3: 0.527400
+% Test accuracy, ensemble model: 0.532600
+
+%% 3. ensemble learning for several cycles
+n_cycle = 10;
+nns = cell(1, n_cycle+1);   % NN cell
+metrics = cell(1, n_cycle);   % metrics cell
+[train_data, valid_data, test_data] = loadData(true, 1000);
+tmp = GD2;
+GD2.n_batch = 100; GD2.lr = 1e-5;
+GD2.cyclic = true; GD2.lr_max = 1e-1;
+GD2.ns = 2 * floor(49000/GD2.n_batch);	% stepsize <-> 2 epochs
+GD2.n_epoch = 4;	% 1 cycle = 4 epochs
+GD2.lambda = 0.0021731;	% best lr
+
+all_metrics = zeros(6, n_cycle * GD2.n_epoch);
+[W, b] = initParam();
+nns{1} = DoubleLayer(W, b);
+
+% each cycle: save model
+for i = 1:n_cycle 
+    [nns{i+1}, metrics{i}] = miniBatchGD(train_data, valid_data, nns{i});
+    all_metrics(:, 1 + GD2.n_epoch * (i-1): GD2.n_epoch * i) = metrics{i};
+end
+
+% plot metrics
+subplotMetrics(all_metrics);
+
+% ensemble
+accs = ensemble(nns(2:end), test_data);
+for i = 1: n_cycle 
+    fprintf("Test accuracy, model %d: %f\n", i, accs{i});
+end
+fprintf("Test accuracy, ensemble model: %f\n", accs{end});
+GD2 = tmp;
